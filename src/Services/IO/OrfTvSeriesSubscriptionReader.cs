613425d4fd735b7768c03b29930af28425b1d917
StefanK <kulmer@gmx.at>
2021-05-02 23:18:18 +0200

    added data class, application service and io

---
+++ src/Services/IO/OrfTvSeriesSubscriptionReader.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+
+namespace AustrianTvScrapper.Services.IO
+{
+    class OrfTvSeriesSubscriptionReader
+    {
+        public IReadOnlyCollection<OrfTvSeriesSubscription> Load(string path)
+        {
+            var jsonString = File.ReadAllText(path);
+            return JsonSerializer.Deserialize<OrfTvSeriesSubscription[]>(jsonString, OrfTvSeriesSubscriptionWriter.JsonOptions);
+        }
+    }
+}
---
+++ src/Services/OrfTvSeriesSubscriptionService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace AustrianTvScrapper.Services
+{
+    public class OrfTvSeriesSubscriptionService
+    {
+        public void AddSubscription(OrfTvSeriesSubscription subscription)
+        {
+            var allSubscriptions = _GetSubscriptions();
+            var newSubscriptions = OrfTvSeriesSubscription.AddSubscription(allSubscriptions, subscription);
+
+            var path = _GetSubscriptionDataPath();
+            var writer = new IO.OrfTvSeriesSubscriptionWriter();
+            writer.Save(path, newSubscriptions);
+        }
+
+        public IReadOnlyCollection<OrfTvSeriesSubscription> GetSubscriptions()
+        {
+            return _GetSubscriptions();
+        }
+
+        private IReadOnlyCollection<OrfTvSeriesSubscription> _GetSubscriptions()
+        {
+            string path = _GetSubscriptionDataPath();
+
+            if (!File.Exists(path))
+            {
+                return new OrfTvSeriesSubscription[0];
+            }
+            var reader = new IO.OrfTvSeriesSubscriptionReader();
+            return reader.Load(path);
+        }
+
+        private static string _GetSubscriptionDataPath()
+        {
+            return Path.Combine(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "AustrianTvScrapper"), "OrfTvSeriesSubscriptions.json");
+        }
+    }
+}
