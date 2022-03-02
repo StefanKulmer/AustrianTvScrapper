9ed69a698041ada8cb95e111936feb641e2b1871
StefanK <kulmer@gmx.at>
2022-03-02 23:27:19 +0100

    adapted episode - extended by some fields

--- src/Services/OrfTvSeriesEpisode.cs
+++ src/Services/OrfTvSeriesEpisode.cs
@@ -5,15 +5,12 @@ namespace AustrianTvScrapper.Services
     public class OrfTvSeriesEpisode
     {
         public OrfTvSeries TvSeries { get; set; }
-
-        public DateTime Date { get; set; }
-
         public string DownloadUrl { get; set; }
-
+        public string Channel { get; set; }
+        public string Profile { get; set; }
         public string Title { get; set; }
-
         public string Description { get; set; }
-
-        public string Description2 { get; set; }
+        public DateTime Date { get; set; }
+        public string Duration { get; set; }
     }
 }
