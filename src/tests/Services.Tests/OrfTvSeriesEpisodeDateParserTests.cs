308a31311f053eb452eedb7a6a32be8a9a7f9a23
StefanK <kulmer@gmx.at>
2022-03-03 23:01:31 +0100

    added tests for date parser

---
+++ src/tests/Services.Tests/OrfTvSeriesEpisodeDateParserTests.cs
@@ -0,0 +1,21 @@
+﻿using Xunit;
+
+namespace AustrianTvScrapper.Services
+{
+    public class OrfTvSeriesEpisodeDateParserTests
+    {
+        [Fact]
+        public void Parse_CentralEuropeanSummerTime()
+        {
+            var parsedDateTime = OrfTvSeriesEpisodeDateParser.Parse("2022-03-02CEST19:00:00");
+            Assert.Equal(new System.DateTime(2022, 03, 2, 19, 0, 0), parsedDateTime);
+        }
+
+        [Fact]
+        public void Parse_CentralEuropeanTime()
+        {
+            var parsedDateTime = OrfTvSeriesEpisodeDateParser.Parse("2021-05-09CET19:42:13");
+            Assert.Equal(new System.DateTime(2021, 5, 9, 19, 42, 13), parsedDateTime);
+        }
+    }
+}
