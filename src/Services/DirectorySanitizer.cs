38b86529443337cc75ee2eb2be7e606a22210796
StefanK <kulmer@gmx.at>
2022-03-07 22:47:19 +0100

    only replace invalid characters for path names and keep special characters like äöü!

--- src/Services/DirectorySanitizer.cs
+++ src/Services/DirectorySanitizer.cs
@@ -4,24 +4,24 @@
     {
         public static string Sanitize(string directoryName)
         {
-            var firstStep = directoryName.Replace("Ä", "Ae")
-                .Replace("Ö", "Oe")
-                .Replace("Ü", "Ue")
-                .Replace("ä", "ae")
-                .Replace("ö", "oe")
-                .Replace("ü", "ue")
-                .Replace("ß", "ss")
-                .Replace("/", "v")
-                .Replace(": ", " - ")
-                .Replace("\"", "")
-                .Replace("–", "-")
-                .Replace("…", "...")
-                .Replace(":", "-");
+            //var firstStep = directoryName.Replace("Ä", "Ae")
+            //    .Replace("Ö", "Oe")
+            //    .Replace("Ü", "Ue")
+            //    .Replace("ä", "ae")
+            //    .Replace("ö", "oe")
+            //    .Replace("ü", "ue")
+            //    .Replace("ß", "ss")
+            //    .Replace("/", "v")
+            //    .Replace(": ", " - ")
+            //    .Replace("\"", "")
+            //    .Replace("–", "-")
+            //    .Replace("…", "...")
+            //    .Replace(":", "-");
 
             string invalidChars = System.Text.RegularExpressions.Regex.Escape(new string(System.IO.Path.GetInvalidFileNameChars()));
             string invalidRegStr = string.Format(@"([{0}]*\.+$)|([{0}]+)", invalidChars);
 
-            return System.Text.RegularExpressions.Regex.Replace(firstStep, invalidRegStr, "_");
+            return System.Text.RegularExpressions.Regex.Replace(directoryName, invalidRegStr, "_");
         }
     }
 }
